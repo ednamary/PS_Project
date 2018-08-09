553bf2a5bb8b27175ad1e0bc7b0bbc94299195b9
Bereket Issack <bereketissack@gmail.com>
2018-08-09 14:22:36 -0700

    bere now
    just now

--- apitest.py
+++ apitest.py
@@ -4,7 +4,7 @@ import time
 import sqlite3
 from pathlib import Path
 
-# New test comment
+#
 
 # This class is to have an easily portable data structure to add things to
 # the database or whatever else is necessary
