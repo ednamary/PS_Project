14dda30032f850fabdd0b201845b5ca2eea53b3f
ogazit <oren.l.gazit@gmail.com>
2018-08-09 14:13:36 -0700

    remove test comment

--- apitest.py
+++ apitest.py
@@ -4,8 +4,6 @@ import time
 import sqlite3
 from pathlib import Path
 
-# Test comment
-
 # This class is to have an easily portable data structure to add things to
 # the database or whatever else is necessary
 class datum():
