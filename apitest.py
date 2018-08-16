ecd2ace9b623397c7dd81b324a91cfe313b22d11
Bereket Issack <bereketissack@gmail.com>
2018-08-16 13:57:06 -0700

    Update apitest.py
    I have included a python code that calculates the last 12 th number digit for each UPC number

--- apitest.py
+++ apitest.py
@@ -95,6 +95,34 @@ def create_input():
     cur = con.cursor()
 
     for line in data:
+        sett = line.rstrip()
+        #Calculating the 12th digit for each of the 11 UPC number completes the complete UPC number 
+        # The 12 upc number helps to find the details of each UPC.
+        #Note: the code will only calculate for 11 digit upcs otherwise the number is considered as invalid
+        totall = 0 #total is the sum of all even number digits
+        total=0 #totall is the sum of all add number digits
+        for i in range(len(sett)):
+            if len(sett) == 11:
+                
+                if i % 2 == 0:
+                    temp = 3*(int(sett[i]))
+                    totall +=temp
+                else:
+                    temp = int(sett[i])
+                    total +=temp 
+                #Sum is the sum of total and totall numbers
+                Sum = totall+total 
+            else:
+                print("UPC number is invalid")
+            
+            # n is the last digit number add to the UPC. 
+            if Sum %10 > 0:
+                n = 10-(Sum%10)
+            else:
+                n = 0
+            ere = ''.join((sett, str(n)))   
+            print(ere)
+        
         # Create a new mini dictionary to use with the Requests syntax
         # To do: find out if it's possible to make one large dictionary and do
         # one large API call instead of a bunch of tiny ones
